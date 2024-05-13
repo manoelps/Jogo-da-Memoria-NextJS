3a20a83102a3c5a7116119df80883ad57ed8798f
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-13 19:32:18 -0300

    atualizado o mock dos cards

---
+++ src/mocks/cards.ts
@@ -0,0 +1,146 @@
+export const cards = [
+  {
+    id: 1,
+    image: 'dog.png',
+    pair: 'dog',
+    visibility: true
+  },
+  {
+    id: 2,
+    image: 'cat.png',
+    pair: 'cat',
+    visibility: true
+  },
+  {
+    id: 3,
+    image: 'dog.png',
+    pair: 'dog',
+    visibility: true
+  },
+  {
+    id: 4,
+    image: 'cat.png',
+    pair: 'cat',
+    visibility: true
+  },
+  {
+    id: 5,
+    image: 'bird.png',
+    pair: 'bird',
+    visibility: true
+  },
+  {
+    id: 6,
+    image: 'monkey.png',
+    pair: 'monkey',
+    visibility: true
+  },
+  {
+    id: 7,
+    image: 'bird.png',
+    pair: 'bird',
+    visibility: true
+  },
+  {
+    id: 8,
+    image: 'monkey.png',
+    pair: 'monkey',
+    visibility: true
+  },
+  {
+    id: 9,
+    image: 'rabbit.png',
+    pair: 'rabbit',
+    visibility: true
+  },
+  {
+    id: 10,
+    image: 'fish.png',
+    pair: 'fish',
+    visibility: true
+  },
+  {
+    id: 11,
+    image: 'rabbit.png',
+    pair: 'rabbit',
+    visibility: true
+  },
+  {
+    id: 12,
+    image: 'fish.png',
+    pair: 'fish',
+    visibility: true
+  },
+  {
+    id: 13,
+    image: 'flower.png',
+    pair: 'flower',
+    visibility: true
+  },
+  {
+    id: 14,
+    image: 'tree.png',
+    pair: 'tree',
+    visibility: true
+  },
+  {
+    id: 15,
+    image: 'flower.png',
+    pair: 'flower',
+    visibility: true
+  },
+  {
+    id: 16,
+    image: 'tree.png',
+    pair: 'tree',
+    visibility: true
+  },
+  {
+    id: 17,
+    image: 'car.png',
+    pair: 'car',
+    visibility: true
+  },
+  {
+    id: 18,
+    image: 'child.png',
+    pair: 'child',
+    visibility: true
+  },
+  {
+    id: 19,
+    image: 'car.png',
+    pair: 'car',
+    visibility: true
+  },
+  {
+    id: 20,
+    image: 'child.png',
+    pair: 'child',
+    visibility: true
+  },
+  {
+    id: 21,
+    image: 'sun.png',
+    pair: 'sun',
+    visibility: true
+  },
+  {
+    id: 22,
+    image: 'moon.png',
+    pair: 'moon',
+    visibility: true
+  },
+  {
+    id: 23,
+    image: 'sun.png',
+    pair: 'sun',
+    visibility: true
+  },
+  {
+    id: 24,
+    image: 'moon.png',
+    pair: 'moon',
+    visibility: true
+  }
+];
