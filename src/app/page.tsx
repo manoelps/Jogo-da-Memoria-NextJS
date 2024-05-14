cd0b9a06db17effa569dcd7b180a8020bc38e3cf
Manoel Pereira dos Santos <manoelps@live.com>
2024-05-13 22:21:56 -0300

    adicionado animacao de transicao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,10 @@ const Home = () => {
             <div
               key={card.id}
               className={classNames(
-                'flex items-center justify-center w-20 h-20 bg-neutral-600 rounded cursor-pointer shadow-md',
+                'flex items-center justify-center w-20 h-20 bg-neutral-600 rounded cursor-pointer shadow-md transition-all duration-500 hover:scale-95',
                 {
-                  'bg-neutral-100 cursor-not-allowed': card.visibility
+                  'bg-neutral-100 cursor-not-allowed hover:scale-100':
+                    card.visibility
                 }
               )}
               onClick={() => {
